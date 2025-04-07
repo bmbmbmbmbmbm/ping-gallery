4c1fc00feb29bcf3a8fca2f65f83ce9275bc7440
Ben M <mckjamin.benenzie@gmail.com>
2025-04-07 06:11:09 +0100

    removed post; using controller to get image meta, which can then be used to get the image

--- server/src/main/kotlin/com/gallery/server/controller/ImageMetaController.kt
+++ server/src/main/kotlin/com/gallery/server/controller/ImageMetaController.kt
@@ -3,14 +3,17 @@ package com.gallery.server.controller
 import com.gallery.server.repository.ImageMetaRepository
 import com.gallery.server.model.ImageMeta
 import org.springframework.web.bind.annotation.RestController
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 
 @RestController
-public class ImageMetaController(private val imageMetaRepository: ImageMetaRepository) {
+public class ImageMetaController() {
     
-    @PostMapping("/image-meta")
-    public fun postImageMeta(@RequestBody imageMeta: ImageMeta) {
-        imageMetaRepository.save(imageMeta)
+    @GetMapping("/image-meta")
+    public fun getImageMeta() {
+    }
+
+    @GetMapping("/image-meta/{id}")
+    public fun getImageMetaById(@PathVariable("id") id: String) {
     }
 }
