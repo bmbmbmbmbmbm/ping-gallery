d6dc856de613b9a003b69be0fd66653f3f9173a0
Ben M <mckjamin.benenzie@gmail.com>
2025-04-07 08:50:09 +0100

    wrong route for image metadata; needed to be image-meta

--- client/src/api.ts
+++ client/src/api.ts
@@ -1,4 +1,4 @@
 const BASE_URL = import.meta.env.VITE_API_URL
 
-export const IMAGE_METADATA_URL = BASE_URL + "/image-metadata"
+export const IMAGE_METADATA_URL = BASE_URL + "/image-meta"
 export const IMAGE_METADATA_ID_URL = IMAGE_METADATA_URL + "/"
